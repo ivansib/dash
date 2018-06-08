3540d9ca1482b142c324c145fcc079d9e202c0c0
TriKriSta <Trikrista13@gmail.com>
2018-06-08 12:42:09 +0300

    add request for a list of unconfirmed

--- src/dex/rpc.cpp
+++ src/dex/rpc.cpp
@@ -886,6 +886,39 @@ UniValue getdexinfo(const UniValue& params, bool fHelp)
     return result;
 }
 
+UniValue dexunconfirmed(const UniValue& params, bool fHelp)
+{
+    if (!fTxIndex) {
+        throw runtime_error(
+            "To use this feture please enable -txindex and make -reindex.\n"
+        );
+    }
+
+    if (dex::DexDB::self() == nullptr) {
+        throw runtime_error(
+            "DexDB is not initialized.\n"
+        );
+    }
+
+    if (fHelp)
+        throw runtime_error(
+            "dexunconfirmed\n"
+            "Return list pair{hash, idTransaction} unconfirmed offers.\n"
+        );
+
+    UniValue result(UniValue::VARR);
+
+    auto unc = dexman.getUncOffers()->getAllOffers();
+    for (auto i : unc) {
+        UniValue v(UniValue::VOBJ);
+        v.push_back(Pair("hash", i.hash.GetHex()));
+        v.push_back(Pair("txid", i.idTransaction.GetHex()));
+        result.push_back(v);
+    }
+
+    return result;
+}
+
 static const CRPCCommand commands[] = // WARNING: check affter merge branches (add parameters if need)
 { //  category              name                        actor (function)           okSafeMode
     //  --------------------- ------------------------    -----------------------    ----------
@@ -899,7 +932,8 @@ static const CRPCCommand commands[] = // WARNING: check affter merge branches (a
     { "dex",    "senddexoffer",   &senddexoffer,   true,  {} },
     { "dex",    "dexsync",        &dexsync,        true,  {} },
     { "dex",    "dexsettings",    &dexsettings,    true,  {} },
-    { "dex",    "getdexinfo",     &getdexinfo,     true,  {} }
+    { "dex",    "getdexinfo",     &getdexinfo,     true,  {} },
+    { "dex",    "dexunconfirmed", &dexunconfirmed, true,  {} }
 };
 
 void RegisterDexRPCCommands(CRPCTable &t)
