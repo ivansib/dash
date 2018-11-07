2878d607082119dce29853080c35a41d85c1c709
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-11-07 10:39:25 +0300

    Add 5th denom, drop deprecated logic and bump min PS version (#2318)
    * Bump PS
    - add 5th denom (0.001)
    - always allow OP_RETURN in PS collaterals
    - collateral amount is always 1/10 of the smallest denom
    - bump MIN_PRIVATESEND_PEER_PROTO_VERSION to 70211
    
    * drop backward compatibility in dsa/dsq serialization
    
    * Use GetSmallestDenomination() in GetCollateralAmount()
    
    * bump MIN_PRIVATESEND_PEER_PROTO_VERSION to 70212

--- src/privatesend.cpp
+++ src/privatesend.cpp
@@ -367,13 +367,8 @@ bool CPrivateSend::IsCollateralValid(const CTransaction& txCollateral)
 
 bool CPrivateSend::IsCollateralAmount(CAmount nInputAmount)
 {
-    if (mnpayments.GetMinMasternodePaymentsProto() > 70208) {
-        // collateral input can be anything between 1x and "max" (including both)
-        return (nInputAmount >= GetCollateralAmount() && nInputAmount <= GetMaxCollateralAmount());
-    } else { // <= 70208
-        // collateral input can be anything between 2x and "max" (including both)
-        return (nInputAmount >= GetCollateralAmount() * 2 && nInputAmount <= GetMaxCollateralAmount());
-    }
+    // collateral input can be anything between 1x and "max" (including both)
+    return (nInputAmount >= GetCollateralAmount() && nInputAmount <= GetMaxCollateralAmount());
 }
 
 /*  Create a nice string to show the denominations
