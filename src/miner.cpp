6bfcc38aafa54bcc533576aaa8220a4dbdb34c09
Alexander Block <ablock84@gmail.com>
2018-11-25 14:27:56 +0100

    Add 0 entry to vTxSigOps when adding quorum commitments (#2489)
    This fixes a crash observed on Travis. The same crash is likely to happen
    outside of Travis as well later.

--- src/miner.cpp
+++ src/miner.cpp
@@ -221,6 +221,7 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
             if (llmq::quorumBlockProcessor->GetMinableCommitmentTx(p.first, pindexPrev, qcTx)) {
                 pblock->vtx.emplace_back(qcTx);
                 pblocktemplate->vTxFees.emplace_back(0);
+                pblocktemplate->vTxSigOps.emplace_back(0);
             }
         }
     }
