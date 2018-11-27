ivansib/sibcoin-old

adding tests for convert price string <-> number

src/qt/test/test_main.cpp
@@ -10,6 +10,7 @@
 #include "util.h"
 #include "uritests.h"
 #include "compattests.h"
+#include "convertdatatests.h"
 
 #ifdef ENABLE_WALLET
 #include "paymentservertests.h"
@@ -48,6 +49,11 @@ int main(int argc, char *argv[])
 #ifdef ENABLE_WALLET
     PaymentServerTests test2;
     if (QTest::qExec(&test2) != 0)
+        fInvalid = true;
+#endif
+#ifdef ENABLE_DEX
+    ConvertDataTests test3;
+    if (QTest::qExec(&test3) != 0)
         fInvalid = true;
 #endif
     CompatTests test4;

ivansib/dash

src/qt/test/test_main.cpp
@@ -14,6 +14,7 @@
 #include "uritests.h"
 #include "compattests.h"
 #include "trafficgraphdatatests.h"
+#include "convertdatatests.h"
 
 #ifdef ENABLE_WALLET
 #include "paymentservertests.h"
@@ -72,6 +73,13 @@ int main(int argc, char *argv[])
     TrafficGraphDataTests test5;
     if (QTest::qExec(&test5) != 0)
         fInvalid = true;
+
+#ifdef ENABLE_DEX
+    ConvertDataTests test6;
+    if (QTest::qExec(&test6) != 0)
+        fInvalid = true;
+#endif
+
     ECC_Stop();
     return fInvalid;
 }