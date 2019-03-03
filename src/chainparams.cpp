8fad2eb05880db42148022c906af7cedab30561b
ivansib <ivansib@users.noreply.github.com>
2019-03-03 12:12:50 +0100

    change RemoveDependencyRewardOnDiffHeight for testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -298,7 +298,7 @@ public:
         consensus.BIP65Height = 66528; // 0000009d43b7778b3057c232afd3c7ce424a17adf8375b8b3ecc0578a4e0f272
         consensus.BIP66Height = 76; // 00000243719923dcc244bbb0e81bd5b03e3ce4fd3fe408358503ae5856ce3b40
         consensus.DIP0001Height = 340100; 
-        consensus.RemoveDependencyRewardOnDiffHeight = 404001;       
+        consensus.RemoveDependencyRewardOnDiffHeight = 405151;       
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // ~uint256(0) >> 20
         consensus.nPowTargetTimespan = 24 * 60 * 60; // Sibcon: 1 day
         consensus.nPowTargetSpacing = 2.5 * 60; // Sibcon: 2.5 minutes
