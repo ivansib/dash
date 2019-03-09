7ac911e5883ac3d92354f3baa8a059609f3df1c4
ivansib <ivansib@users.noreply.github.com>
2019-03-09 10:44:17 +0100

    change default theme to light theme

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -929,7 +929,7 @@ QString getThemeName()
     if(!theme.isEmpty()){
         return theme;
     }
-    return QString("trad");
+    return QString("light");
 }
 
 // Open CSS when configured
@@ -944,8 +944,8 @@ QString loadStyleSheet()
         cssName = QString(":/css/") + theme; 
     }
     else {
-        cssName = QString(":/css/trad");  
-        settings.setValue("theme", "trad");
+        cssName = QString(":/css/light");  
+        settings.setValue("theme", "light");
     }
     
     QFile qFile(cssName);      
