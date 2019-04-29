96d00dc738faaf358e85747445b3b2dfb450f3d0
UdjinM6 <UdjinM6@users.noreply.github.com>
2019-04-29 11:32:43 +0300

    New LLMQ-based IS should have no legacy IS strings in UI and no legacy restrictions (#2883)
    (max value/too many inputs/additional fees)

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -232,6 +232,8 @@ public:
     int getDefaultConfirmTarget() const;
     int getNumISLocks() const;
 
+    bool IsOldInstantSendEnabled() const;
+
 private:
     CWallet *wallet;
     bool fHaveWatchOnly;
