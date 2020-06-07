1a1f7da601d1f7bc36ed1239efb666643a1ebc90
ivansib <ivansib@users.noreply.github.com>
2020-06-07 14:19:19 +0100

    Bump version to 0.17

--- src/clientversion.h
+++ src/clientversion.h
@@ -15,8 +15,8 @@
 
 // These need to be macros, as version.cpp's and sibcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
-#define CLIENT_VERSION_MINOR       16
-#define CLIENT_VERSION_REVISION    4
+#define CLIENT_VERSION_MINOR       17
+#define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
 //! Set to true for release, false for prerelease or test build
